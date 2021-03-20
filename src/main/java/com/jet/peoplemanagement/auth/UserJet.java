b1445bece7813dcf19d77869c55a7da6b97e6af9
4008924 <alex.gomes@riachuelo.com.br>
2021-03-20 19:57:00 -0300

    Adicionando autenticação com  JWT

---
+++ src/main/java/com/jet/peoplemanagement/auth/UserJet.java
@@ -0,0 +1,13 @@
+package com.jet.peoplemanagement.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserJet {
+
+    private String email;
+    private String password;
+
+}
